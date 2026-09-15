9350b09f57dce55f1d1e879b715a2164bce510d6
nikicoolgame <nikicoolgame@web.de>
2026-09-15 16:12:21 +0000

    V1

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.NUTRIFY_CONFIG = {
   // Einmalig mit den Werten aus Supabase Project Settings -> API ausfüllen.
-  url: "",
-  key: ""
+  url: "https://ubqvapducicxpjfvxqpc.supabase.co",
+  key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVicXZhcGJ1Y2ljeHBqZnZ4cHFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkxNDQ5MDcsImV4cCI6MjEwNDcyMDkwN30.bHP6ROETOaLjG7pWM0ZmRducFoGt6rGxcr0Oosigp8s"
 };
